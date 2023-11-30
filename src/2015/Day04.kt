069d104be441418c38d66bd1a47c9e43c6f57bf5
Scott Busche <swbusche@gmail.com>
2023-11-29 21:56:50 -0600

    Day 4

---
+++ src/2015/Day04.kt
@@ -0,0 +1,25 @@
+package `2015`
+
+import md5
+import println
+
+fun main() {
+    fun findHash(secretKey: String, prefix: String): Int {
+        return generateSequence(0) { it + 1 }.first {
+            "$secretKey$it".md5().startsWith(prefix)
+        }
+    }
+
+    // MD5s that start with 00000
+    fun part1(secretKey: String): Int {
+        return findHash(secretKey, "00000")
+    }
+    check(part1("abcdef") == 609043)
+    check(part1("pqrstuv") == 1048970)
+
+    fun part2(secretKey: String): Int {
+        return findHash(secretKey, "000000")
+    }
+    part1("iwrupvqb").println()
+    part2("iwrupvqb").println()
+}
