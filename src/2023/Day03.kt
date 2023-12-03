c5aac89b718975614ff303746bf6d16706e11707
Scott Busche <swbusche@gmail.com>
2023-12-03 09:14:36 -0600

    2023 - Day 3 - Part 2

--- src/2023/Day03.kt
+++ src/2023/Day03.kt
@@ -17,27 +17,31 @@ fun main() {
         return coordinates
     }
 
+    fun wrapInSearchCoordinates(
+        coordinates: Pair<Int, Int>,
+        matrix: List<List<Char>>
+    ) =
+        listOf(
+            // Row Above
+            Pair(coordinates.first - 1, coordinates.second - 1),
+            Pair(coordinates.first - 1, coordinates.second),
+            Pair(coordinates.first - 1, coordinates.second + 1),
+            // Current Row
+            Pair(coordinates.first, coordinates.second - 1),
+            Pair(coordinates.first, coordinates.second + 1),
+            // Row Below
+            Pair(coordinates.first + 1, coordinates.second - 1),
+            Pair(coordinates.first + 1, coordinates.second),
+            Pair(coordinates.first + 1, coordinates.second + 1),
+        )
+            .filter { coordinates ->
+                coordinates.first >= 0 && coordinates.first < matrix.size &&
+                        coordinates.second >= 0 && coordinates.second < matrix[0].size
+            }
+
     fun hasAdjacentSymbol(matrix: List<List<Char>>, partNumberCoordinates: List<Pair<Int, Int>>): Boolean {
         return partNumberCoordinates
-            .flatMap { coordinates ->
-                listOf(
-                    // Row Above
-                    Pair(coordinates.first - 1, coordinates.second - 1),
-                    Pair(coordinates.first - 1, coordinates.second),
-                    Pair(coordinates.first - 1, coordinates.second + 1),
-                    // Current Row
-                    Pair(coordinates.first, coordinates.second - 1),
-                    Pair(coordinates.first, coordinates.second + 1),
-                    // Row Below
-                    Pair(coordinates.first + 1, coordinates.second - 1),
-                    Pair(coordinates.first + 1, coordinates.second),
-                    Pair(coordinates.first + 1, coordinates.second + 1),
-                )
-            }
-            .filter { coordinates ->
-                coordinates.first > 0 && coordinates.first < matrix.size &&
-                        coordinates.second > 0 && coordinates.second < matrix[0].size
-            }
+            .flatMap { wrapInSearchCoordinates(it, matrix) }
             .any { coordinates ->
                 val character = matrix[coordinates.first][coordinates.second]
                 !(character.isDigit() || character == '.')
@@ -45,7 +49,7 @@ fun main() {
 
     }
 
-    fun getPartList(matrix: List<List<Char>>): List<List<Pair<Int, Int>>> {
+    fun getPartListCoordinates(matrix: List<List<Char>>): List<List<Pair<Int, Int>>> {
         val partList = mutableListOf<List<Pair<Int, Int>>>()
 
         for (x in 0..<(matrix.size)) {
@@ -68,9 +72,35 @@ fun main() {
         return partList
     }
 
+    fun getPartNumbersOfGear(matrix: List<List<Char>>, partList: List<List<Pair<Int, Int>>>): List<List<Int>> {
+        val gearList = mutableListOf<List<Int>>()
+
+        for (x in 0..<(matrix.size)) {
+            val line = matrix[x]
+            var y = 0
+            while (y < line.size) {
+                if (line[y] == '*') {
+                    val gearCoordinates = Pair(x, y)
+                    val searchCoordinates = wrapInSearchCoordinates(gearCoordinates, matrix)
+                    val partsNextToGear = partList.filter { partCoordinates ->
+                        searchCoordinates.any { partCoordinates.contains(it) }
+                    }.map { partCoordinates ->
+                        partCoordinates.map { matrix[it.first][it.second] }.joinToString("").toInt()
+                    }
+                    if (partsNextToGear.size > 1) {
+                        gearList.add(partsNextToGear)
+                    }
+                }
+                y++
+            }
+        }
+        return gearList
+    }
+
+
     fun part1(input: List<String>): Int {
         val matrix = input.map { line -> line.toList() }
-        return getPartList(matrix)
+        return getPartListCoordinates(matrix)
             .sumOf { partList -> partList.map { matrix[it.first][it.second] }.joinToString("").toInt() }
     }
 
@@ -91,22 +121,33 @@ fun main() {
         ) == 4361
     )
 
-
-    fun part2(input: List<String>): Int {
-        TODO()
-    }
-
-//    check(
-//        part2(
-//            listOf(
-//                "London to Dublin = 464",
-//                "London to Belfast = 518",
-//                "Dublin to Belfast = 141"
-//            )
-//        ) == 982
-//    )
-
     val input = readInput("2023/Day03")
     part1(input).println()
+
+
+    fun part2(input: List<String>): Int {
+        val matrix = input.map { line -> line.toList() }
+        val partList = getPartListCoordinates(matrix)
+        return getPartNumbersOfGear(matrix, partList)
+            .sumOf { it.first() * it.last() }
+    }
+
+    check(
+        part2(
+            listOf(
+                "467..114..",
+                "...*......",
+                "..35..633.",
+                "......#...",
+                "617*......",
+                ".....+.58.",
+                "..592.....",
+                "......755.",
+                "...\$.*....",
+                ".664.598.."
+            )
+        ) == 467835
+    )
+
     part2(input).println()
 }
