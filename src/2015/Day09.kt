4710b87b67489aaed4f11a8e5e3c829c1682db68
Scott Busche <swbusche@gmail.com>
2023-12-03 21:10:14 -0600

    Extra permutations

--- src/2015/Day09.kt
+++ src/2015/Day09.kt
@@ -2,6 +2,7 @@ package `2015`
 
 import println
 import readInput
+import utils.permutations
 
 fun main() {
 
@@ -61,17 +62,3 @@ fun main() {
     part1(input).println()
     part2(input).println()
 }
-
-
-// Based off https://gist.github.com/trygvea/a2d9cdbc19ceff3df7eb64ccef3c0597
-fun <T> List<T>.permutations(): List<List<T>> = when {
-    size > 10 -> throw Exception("You probably dont have enough memory to keep all those permutations")
-    size <= 1 -> listOf(this)
-    else ->
-        drop(1).permutations().map { perm ->
-            (indices).map { i ->
-                perm.subList(0, i) + first() + perm.drop(i)
-            }
-        }.flatten()
-}
-
---
+++ src/utils/Permutations.kt
@@ -0,0 +1,13 @@
+package utils
+
+// Based off https://gist.github.com/trygvea/a2d9cdbc19ceff3df7eb64ccef3c0597
+fun <T> List<T>.permutations(): List<List<T>> = when {
+    size > 10 -> throw Exception("You probably dont have enough memory to keep all those permutations")
+    size <= 1 -> listOf(this)
+    else ->
+        drop(1).permutations().map { perm ->
+            (indices).map { i ->
+                perm.subList(0, i) + first() + perm.drop(i)
+            }
+        }.flatten()
+}
