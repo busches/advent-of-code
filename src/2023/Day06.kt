fb2d471b0344da80120836a1de1cf3134a46597e
Scott Busche <swbusche@gmail.com>
2023-12-06 18:28:59 -0600

    2023 - Day 6 - Part 2

--- src/2023/Day06.kt
+++ src/2023/Day06.kt
@@ -6,28 +6,26 @@ import readInput
 import utils.transpose
 
 fun main() {
-
-    // Time:      7  15   30
-    //Distance:  9  40  200
-
     data class Race(val time: Long, val distance: Long)
 
-    fun extractValues(input: List<String>) : List<Race> {
+    fun extractValuesPart1(input: List<String>): List<Race> {
         return input.map { it.extractLongs().toList() }.transpose()
             .map { Race(it[0], it[1]) }
     }
 
-    fun part1(input: List<String>): Int {
-        return extractValues(input).map {(time, distance) ->
-            (1..<time).filter { holdButtonLength ->
+    fun waysToWinMultiplied(blah: List<Race>) = blah
+        .map { (time, distance) ->
+            (1..<time).count { holdButtonLength ->
                 val timeRemaining = time - holdButtonLength
                 val distanceTraveled = timeRemaining * holdButtonLength
                 distanceTraveled > distance
             }
-                .count()
-        }.fold(1) {acc, waysToWin ->
+        }.fold(1) { acc, waysToWin ->
             acc * waysToWin
         }
+
+    fun part1(input: List<String>): Int {
+        return waysToWinMultiplied(extractValuesPart1(input))
     }
 
     val testInput = readInput("2023/Day06_Test")
@@ -36,20 +34,18 @@ fun main() {
     val input = readInput("2023/Day06")
     part1(input).println()
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun extractValuesPart2(input: List<String>): List<Race> {
+        val rawRaceValues = input.map { it.extractLongs().toList().fold("") { acc, num -> acc + num } }
+            .map { it.toLong() }
+
+        return listOf(Race(rawRaceValues[0], rawRaceValues[1]))
     }
 
-    check(
-        part2(
-            listOf(
-                "London to Dublin = 464",
-                "London to Belfast = 518",
-                "Dublin to Belfast = 141"
-            )
-        ) == 982
-    )
 
+    fun part2(input: List<String>): Int {
+        return waysToWinMultiplied(extractValuesPart2(input))
+    }
 
+    check(part2(testInput) == 71503)
     part2(input).println()
 }
