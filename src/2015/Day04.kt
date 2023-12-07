2f27bd096fe2ee34c062aaa2820066382d1d5975
Scott Busche <swbusche@gmail.com>
2023-12-06 18:35:03 -0600

    2023 - Day 6 - Part 2

--- src/2015/Day04.kt
+++ src/2015/Day04.kt
@@ -2,6 +2,7 @@ package `2015`
 
 import md5
 import println
+import readInput
 
 fun main() {
     fun findHash(secretKey: String, prefix: String): Int {
@@ -14,12 +15,13 @@ fun main() {
     fun part1(secretKey: String): Int {
         return findHash(secretKey, "00000")
     }
+    val input = readInput("2015/Day04")
     check(part1("abcdef") == 609043)
     check(part1("pqrstuv") == 1048970)
 
     fun part2(secretKey: String): Int {
         return findHash(secretKey, "000000")
     }
-    part1("iwrupvqb").println()
-    part2("iwrupvqb").println()
+    part1(input.first()).println()
+    part2(input.first()).println()
 }
