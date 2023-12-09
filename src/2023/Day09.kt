441917a0c20b077620d2633a47cb234584288573
Scott Busche <swbusche@gmail.com>
2023-12-09 12:54:14 -0600

    2023 - Day 9 - Part 2 - it's gross

--- src/2023/Day09.kt
+++ src/2023/Day09.kt
@@ -13,20 +13,18 @@ fun main() {
 
 
     fun part1(input: List<String>): Long {
-        return extractSequences(input)
-            .map { startingSequence ->
-                val allSequences = mutableListOf(startingSequence)
-                while (!allSequences.last().all { it == 0L }) {
-                    val nextSequence = allSequences.last().zipWithNext()
-                        .map { (first, second) -> (second - first) }
+        return extractSequences(input).sumOf { startingSequence ->
+            val allSequences = mutableListOf(startingSequence)
+            while (!allSequences.last().all { it == 0L }) {
+                val nextSequence = allSequences.last().zipWithNext()
+                    .map { (first, second) -> (second - first) }
 
-                    allSequences.add(nextSequence)
-                }
-
-                allSequences.reversed()
-                    .map { it.last() }.fold(0L, Long::plus)
+                allSequences.add(nextSequence)
             }
-            .sum()
+
+            allSequences.reversed()
+                .map { it.last() }.fold(0L, Long::plus)
+        }
     }
 
     check(part1(readInput("2023/Day09_Test")) == 114L)
@@ -36,9 +34,28 @@ fun main() {
 
 
     fun part2(input: List<String>): Long {
-        TODO()
+        return extractSequences(input).map { it.reversed() }.sumOf { startingSequence ->
+            val allSequences = mutableListOf(startingSequence)
+            while (!allSequences.last().all { it == 0L }) {
+                val nextSequence = allSequences.last().zipWithNext()
+                    .map { (first, second) -> (first - second) }
+
+                allSequences.add(nextSequence)
+            }
+
+            val finalNumbers = allSequences.reversed().map { it.last() }
+            val newNumbers = mutableListOf(0L)
+
+            finalNumbers.map {
+                val nextNumber = it - newNumbers.last()
+                newNumbers.add(nextNumber)
+                nextNumber
+            }
+
+            newNumbers.last()
+        }
     }
 
-    check(part2(readInput("2023/Day09_Test2")) == 6L)
+    check(part2(readInput("2023/Day09_Test")) == 2L)
     part2(input).println()
 }
