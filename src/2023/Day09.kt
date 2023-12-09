1ad3384f634b0d4d8e2fd0f01dda8d7bb607aa61
Scott Busche <swbusche@gmail.com>
2023-12-09 13:36:46 -0600

    Easy peasy now

--- src/2023/Day09.kt
+++ src/2023/Day09.kt
@@ -10,8 +10,8 @@ fun main() {
         return input.map { it.extractLongs().toList() }
     }
 
-    fun part1(input: List<String>): Long {
-        return extractSequences(input).sumOf { startingSequence ->
+    fun List<List<Long>>.solve() =
+        sumOf { startingSequence ->
             generateSequence(startingSequence) { sequence ->
                 sequence.zipWithNext().map { (first, second) -> (second - first) }
             }
@@ -19,6 +19,9 @@ fun main() {
                 .map { it.last() }
                 .fold(0L, Long::plus)
         }
+
+    fun part1(input: List<String>): Long {
+        return extractSequences(input).solve()
     }
 
     check(part1(readInput("2023/Day09_Test")) == 114L)
@@ -26,16 +29,8 @@ fun main() {
     val input = readInput("2023/Day09")
     part1(input).println()
 
-
     fun part2(input: List<String>): Long {
-        return extractSequences(input).map { it.reversed() } .sumOf { startingSequence ->
-            generateSequence(startingSequence) { sequence ->
-                sequence.zipWithNext().map { (first, second) -> (second - first) }
-            }
-                .takeWhile { sequence -> sequence.any { it != 0L } }
-                .map { it.last() }
-                .fold(0L, Long::plus)
-        }
+        return extractSequences(input).map { it.reversed() }.solve()
     }
 
     check(part2(readInput("2023/Day09_Test")) == 2L)
