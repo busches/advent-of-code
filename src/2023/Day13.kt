5b47adf67729e7b33a97f06cb5b143bd7f115057
Scott Busche <swbusche@gmail.com>
2023-12-16 16:49:50 -0600

    2023 - Day 13 - Part 2 - Blargh

--- src/2023/Day13.kt
+++ src/2023/Day13.kt
@@ -31,6 +31,7 @@ fun main() {
 
 
     fun getReflectionLine(newPuzzle: List<String>): Int {
+//        "Searching line in this puzzle\n${newPuzzle.joinToString("\n")}".println()
         for (index in 0..(newPuzzle.size - 2)) {
             if (newPuzzle[index] == newPuzzle[index + 1]) {
                 "$index and ${index + 1} match".println()
@@ -55,13 +56,14 @@ fun main() {
     }
 
     fun scorePuzzle(puzzle: Puzzle): Puzzle {
-        val verticalPuzzle = puzzle.rows.map { it.toList() }.transpose().map { it.joinToString("") }
-        var score = getReflectionLine(verticalPuzzle)
+        var score = getReflectionLine(puzzle.rows) * 100
+        "Horizontal Score $score".println()
 
-        "Vertical Score $score".println()
         if (score == 0) {
-            score = getReflectionLine(puzzle.rows) * 100
-            "Horizontal Score $score".println()
+            val verticalPuzzle = puzzle.rows.map { it.toList() }.transpose().map { it.joinToString("") }
+            var score = getReflectionLine(verticalPuzzle)
+
+            "Vertical Score $score".println()
         }
         return puzzle.copy(part1Score = score)
     }
@@ -73,13 +75,41 @@ fun main() {
             }
     }
 
-    check(part1(readInput("2023/Day13_Test")) == 405)
+//    check(part1(readInput("2023/Day13_Test")) == 405)
 
     val input = readInput("2023/Day13")
-    part1(input).println()
+//    part1(input).println()
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val originalPuzzles = extractPuzzles(input).map { puzzle -> scorePuzzle(puzzle) }
+
+        return originalPuzzles.mapIndexed{ index, puzzle ->
+            "".println()
+            "Puzzle $index".println()
+            "".println()
+            for (row in puzzle.rows.indices) {
+                for (column in puzzle.rows[row].indices) {
+                    val character = puzzle.rows[row][column]
+                    val newRow = StringBuilder(puzzle.rows[row]).apply {setCharAt(column, if (character == '#') '.' else '#')}.toString()
+                    val newPuzzleRows = puzzle.rows.mapIndexed { index, puzzleRow ->
+                        if (index == row) {
+                            newRow
+                        } else {
+                            puzzleRow
+                        }
+                    }
+
+                    val newScore = scorePuzzle(puzzle.copy(rows = newPuzzleRows).also { "Updated Puzzle for $row, $column\n $it".println() }).part1Score
+                    if (newScore > 0 && newScore != puzzle.part1Score) {
+                        return@mapIndexed newScore.also { "Found new score for Puzzle $index".println() }
+                    }
+                }
+            }
+//            puzzle.part1Score
+            // too low 27900 - 0
+            // too high 44300 - puzzle.part1Score
+            throw IllegalArgumentException("Didn't find a new score for $index $puzzle")
+        }.sum()
     }
 
     check(part2(readInput("2023/Day13_Test")) == 400)
