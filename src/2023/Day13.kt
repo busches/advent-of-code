4c4740e580109deb5cf64bcaaf5594a14c9b1044
Scott Busche <swbusche@gmail.com>
2023-12-16 18:15:37 -0600

    2023 - Day 13 - Revert a lot back to Part 1, we're gonna start over

--- src/2023/Day13.kt
+++ src/2023/Day13.kt
@@ -6,7 +6,7 @@ import utils.transpose
 
 fun main() {
 
-    data class Puzzle(val rows: List<String>, val part1Score: Int = -1) {
+    data class Puzzle(val rows: List<String>) {
         override fun toString(): String {
             return "Puzzle\n${rows.joinToString("\n")}"
         }
@@ -29,24 +29,17 @@ fun main() {
         return puzzles
     }
 
-
     fun getReflectionLine(newPuzzle: List<String>): Int {
-//        "Searching line in this puzzle\n${newPuzzle.joinToString("\n")}".println()
         for (index in 0..(newPuzzle.size - 2)) {
             if (newPuzzle[index] == newPuzzle[index + 1]) {
-//                "$index and ${index + 1} match".println()
                 for (nextRowCounter in 1..<(newPuzzle.size - index)) {
                     val topRow = index - nextRowCounter
                     val bottomRow = index + 1 + nextRowCounter
                     if (topRow !in newPuzzle.indices || bottomRow !in newPuzzle.indices) {
-//                        "Reached the end, all rows matched, ${newPuzzle.indices}, $topRow $bottomRow".println()
                         return index + 1
                     } else {
                         if (newPuzzle[topRow] != newPuzzle[bottomRow]) {
-//                            "Rows $topRow and $bottomRow did not match".println()
                             break
-                        } else {
-//                            "Rows $topRow and $bottomRow matched".println()
                         }
                     }
                 }
@@ -55,24 +48,17 @@ fun main() {
         return 0
     }
 
-    fun scorePuzzle(puzzle: Puzzle): Puzzle {
+    fun scorePuzzle(puzzle: Puzzle): Int {
         var score = getReflectionLine(puzzle.rows) * 100
-//        "Horizontal Score $score".println()
-
         if (score == 0) {
             val verticalPuzzle = puzzle.rows.map { it.toList() }.transpose().map { it.joinToString("") }
             score = getReflectionLine(verticalPuzzle)
-
-//            "Vertical Score $score".println()
         }
-        return puzzle.copy(part1Score = score)
+        return score
     }
 
     fun part1(input: List<String>): Int {
-        return extractPuzzles(input)
-            .sumOf { puzzle ->
-                scorePuzzle(puzzle).part1Score//.also { it.println() }
-            }
+        return extractPuzzles(input).sumOf { puzzle -> scorePuzzle(puzzle) }
     }
 
     check(part1(readInput("2023/Day13_Test")) == 405)
@@ -81,35 +67,7 @@ fun main() {
     part1(input).println()
 
     fun part2(input: List<String>): Int {
-        val originalPuzzles = extractPuzzles(input).map { puzzle -> scorePuzzle(puzzle) }
-
-        return originalPuzzles.mapIndexed{ index, puzzle ->
-            "".println()
-            "Puzzle $index".println()
-            "".println()
-            for (row in puzzle.rows.indices) {
-                for (column in puzzle.rows[row].indices) {
-                    val character = puzzle.rows[row][column]
-                    val newRow = StringBuilder(puzzle.rows[row]).apply {setCharAt(column, if (character == '#') '.' else '#')}.toString()
-                    val newPuzzleRows = puzzle.rows.mapIndexed { index, puzzleRow ->
-                        if (index == row) {
-                            newRow
-                        } else {
-                            puzzleRow
-                        }
-                    }
-
-                    val newScore = scorePuzzle(puzzle.copy(rows = newPuzzleRows).also { "Updated Puzzle for $row, $column\n $it".println() }).part1Score
-                    if (newScore > 0 && newScore != puzzle.part1Score) {
-                        return@mapIndexed newScore.also { "Found new score for Puzzle $index".println() }
-                    }
-                }
-            }
-//            puzzle.part1Score
-            // too low 27900 - 0
-            // too high 44300 - puzzle.part1Score
-            throw IllegalArgumentException("Didn't find a new score for $index $puzzle")
-        }.sum()
+        TODO()
     }
 
     check(part2(readInput("2023/Day13_Test")) == 400)
