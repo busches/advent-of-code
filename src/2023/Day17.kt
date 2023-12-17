c045fbb154790ddec053de06d9fccfbe62ac9919
Scott Busche <swbusche@gmail.com>
2023-12-17 15:38:50 -0600

    2023 - Day 17 - Cleanup the duplication a bit

--- src/2023/Day17.kt
+++ src/2023/Day17.kt
@@ -19,13 +19,22 @@ class Day17 {
         UP(-1, 0), DOWN(1, 0), LEFT(0, -1), RIGHT(0, 1),
     }
 
-    data class Crucible(
-        val y: Int,
-        val x: Int,
-        val direction: Direction,
-        val directionDistance: Int,
-    ) {
-        fun nextMoves(): List<Direction> {
+    interface Crucible {
+        val y: Int
+        val x: Int
+        val direction: Direction
+        val directionDistance: Int
+        fun nextMoves(): List<Direction>
+        fun move(direction: Direction): Crucible
+    }
+
+    data class FirstCrucible(
+        override val y: Int,
+        override val x: Int,
+        override val direction: Direction,
+        override val directionDistance: Int,
+    ) : Crucible {
+        override fun nextMoves(): List<Direction> {
             val nextDirections = mutableListOf<Direction>()
             when (direction) {
                 Direction.UP, Direction.DOWN -> {
@@ -45,19 +54,19 @@ class Day17 {
             return nextDirections
         }
 
-        fun move(direction: Direction): Crucible {
+        override fun move(direction: Direction): FirstCrucible {
             val newDistance = if (direction == this.direction) directionDistance + 1 else 1
-            return Crucible(y + direction.changeY, x + direction.changeX, direction, newDistance)
+            return FirstCrucible(y + direction.changeY, x + direction.changeX, direction, newDistance)
         }
     }
 
     data class UltraCrucible(
-        val y: Int,
-        val x: Int,
-        val direction: Direction,
-        val directionDistance: Int,
-    ) {
-        fun nextMoves(): List<Direction> {
+        override val y: Int,
+        override val x: Int,
+        override val direction: Direction,
+        override val directionDistance: Int,
+    ) : Crucible {
+        override fun nextMoves(): List<Direction> {
             val nextDirections = mutableListOf<Direction>()
 
             if (directionDistance > 3) {
@@ -82,60 +91,32 @@ class Day17 {
             return nextDirections
         }
 
-        fun move(direction: Direction): UltraCrucible {
+        override fun move(direction: Direction): UltraCrucible {
             val newDistance = if (direction == this.direction) directionDistance + 1 else 1
             return UltraCrucible(y + direction.changeY, x + direction.changeX, direction, newDistance)
         }
     }
 
     fun part1(input: List<String>): Int {
-        val grid = input.map { it.toList() }
-
-        val start = Crucible(0, 0, Direction.RIGHT, 0)
-        // Use Priority Queue, not a LinkedList, so we can visit the lowest cost values and cache them first, otherwise
-        // We will cache non-optimal paths and never visit them again :)
-        val queue =
-            PriorityQueue<IndexedValue<Crucible>>(compareBy { (cost, crucible) -> cost - crucible.x - crucible.y })
-        val visited = mutableSetOf<Crucible>()
-        queue.add(IndexedValue(0, start))
-
-        while (queue.isNotEmpty()) {
-            val (cost, crucible) = queue.remove()
-
-            if (!visited.add(crucible)) {
-                continue // Already been here
-            }
-
-            if (crucible.x == grid.first().lastIndex && crucible.y == grid.lastIndex) {
-                // We can return here, as we're using the priority queue based on cost and the first exit, should be cheapest
-                return cost
-            }
-
-            for (direction in crucible.nextMoves()) {
-                val newCrucible = crucible.move(direction)
-                // Bounds check
-                if (newCrucible.x !in grid.first().indices || newCrucible.y !in grid.indices) {
-                    continue
-                }
-                val costOfSpace = grid[newCrucible.y][newCrucible.x].digitToInt()
-                val newCost = cost + costOfSpace
-                queue.add(IndexedValue(newCost, newCrucible))
-            }
-        }
-
-        TODO("Didn't finish?")
+        return findCost(input, ::FirstCrucible)
     }
 
     fun part2(input: List<String>): Int {
-        // Duplication FTW
+        return findCost(input, ::UltraCrucible)
+    }
+
+    private inline fun <reified T : Crucible> findCost(
+        input: List<String>,
+        factory: (Int, Int, Direction, Int) -> T
+    ): Int {
         val grid = input.map { it.toList() }
 
-        val start = UltraCrucible(0, 0, Direction.RIGHT, 0)
+        val start = factory(0, 0, Direction.RIGHT, 0)
         // Use Priority Queue, not a LinkedList, so we can visit the lowest cost values and cache them first, otherwise
         // We will cache non-optimal paths and never visit them again :)
         val queue =
-            PriorityQueue<IndexedValue<UltraCrucible>>(compareBy { (cost, crucible) -> cost - crucible.x - crucible.y })
-        val visited = mutableSetOf<UltraCrucible>()
+            PriorityQueue<IndexedValue<T>>(compareBy { (cost, crucible) -> cost - crucible.x - crucible.y })
+        val visited = mutableSetOf<T>()
         queue.add(IndexedValue(0, start))
 
         while (queue.isNotEmpty()) {
@@ -158,12 +139,9 @@ class Day17 {
                 }
                 val costOfSpace = grid[newCrucible.y][newCrucible.x].digitToInt()
                 val newCost = cost + costOfSpace
-                queue.add(IndexedValue(newCost, newCrucible))
+                queue.add(IndexedValue(newCost, newCrucible as T))
             }
         }
-
         TODO("Didn't finish?")
     }
-
-
 }
