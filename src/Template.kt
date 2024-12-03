341a9be15a66130582f2c638964ba035e417754e
Scott Busche <swbusche@gmail.com>
2024-12-03 17:56:06 -0600

    Update template

--- src/Template.kt
+++ src/Template.kt
@@ -7,32 +7,21 @@ fun main() {
         TODO()
     }
 
-    check(
-        part1(
-            listOf(
-                "London to Dublin = 464",
-                "London to Belfast = 518",
-                "Dublin to Belfast = 141"
-            )
-        ) == 605
-    )
-
 
     fun part2(input: List<String>): Int {
         TODO()
     }
 
-    check(
-        part2(
-            listOf(
-                "London to Dublin = 464",
-                "London to Belfast = 518",
-                "Dublin to Belfast = 141"
-            )
-        ) == 982
+    val sampleInput = listOf(
+        "London to Dublin = 464",
+        "London to Belfast = 518",
+        "Dublin to Belfast = 141"
     )
+    check(part1(sampleInput) == 605)
 
     val input = readInput("2015/Day09")
     part1(input).println()
+
+    check(part2(sampleInput) == 982)
     part2(input).println()
 }
