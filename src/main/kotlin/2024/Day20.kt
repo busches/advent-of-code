b0947cad8151a2d4af31d2df851a53052abc1bed
Scott Busche <swbusche@gmail.com>
2024-12-20 22:40:09 -0600

    Day 20 - Part 2 - pretty gross but whatever

--- src/main/kotlin/2024/Day20.kt
+++ src/main/kotlin/2024/Day20.kt
@@ -3,11 +3,12 @@ package `2024`
 import utils.println
 import utils.readInput
 import java.util.*
+import kotlin.math.absoluteValue
 
 
 class Day20 {
     @JvmInline
-    value class Coordinate(private val coordinates: Pair<Int, Int>) {
+    value class Coordinate(val coordinates: Pair<Int, Int>) {
         operator fun plus(other: Coordinate) =
             Coordinate(coordinates.first + other.coordinates.first to coordinates.second + other.coordinates.second)
     }
@@ -39,6 +40,34 @@ class Day20 {
                 copy(position = secondPosition, steps = steps + 2, moves = moves + position + firstPosition)
             }
         }
+
+        private val totalCheats = 20
+
+        // Now we're really cheating
+        fun getMegaCheatMoves(): Collection<Move> {
+            return (-totalCheats..totalCheats).flatMap { xOffset ->
+                (-totalCheats..totalCheats).mapNotNull { yOffset ->
+                    if (xOffset == 0 && yOffset == 0) {
+                        null // skip doing nothing
+                    } else {
+                        val cheats = xOffset.absoluteValue + yOffset.absoluteValue
+                        if (cheats > totalCheats) {
+                            // can't cheat this much
+                            null
+                        } else {
+                            val newPosition =
+                                Coordinate(position.coordinates.first + xOffset to position.coordinates.second + yOffset)
+                            copy(
+                                position = newPosition,
+                                steps = steps + cheats
+                            ) // We don't have the moves anymore, so we split solve into two gross methods
+                        }
+
+                    }
+                }
+            }
+        }
+
     }
 
     private fun buildMap(input: List<String>): Map<Coordinate, Char> {
@@ -52,7 +81,7 @@ class Day20 {
         return map
     }
 
-    private fun solve(
+    private fun solvePath(
         map: Map<Coordinate, Char>,
         startingMove: Move,
         endingPoint: Coordinate,
@@ -61,7 +90,7 @@ class Day20 {
         val remainingMoves = PriorityQueue<Move>(compareBy { move -> move.steps })
         remainingMoves.add(startingMove)
 
-        val visited = mutableSetOf<Pair<Coordinate, List<Coordinate>>>()
+        val visited = mutableSetOf<Coordinate>()
         while (remainingMoves.isNotEmpty()) {
             val move = remainingMoves.remove()
             val whatsHere = map[move.position]
@@ -77,7 +106,40 @@ class Day20 {
                 continue // can't walk into a wall
             } else if (move.position == endingPoint) {
                 return move.moves
-            } else if (!visited.add(move.position to move.moves)) {
+            } else if (!visited.add(move.position)) {
+                continue // already been here
+            }
+            remainingMoves.addAll(move.getNextMoves())
+        }
+        return null
+    }
+
+    private fun solveSteps(
+        map: Map<Coordinate, Char>,
+        startingMove: Move,
+        endingPoint: Coordinate,
+        fastestSolveWithCheating: Int
+    ): Int? {
+        val remainingMoves = PriorityQueue<Move>(compareBy { move -> move.steps })
+        remainingMoves.add(startingMove)
+
+        val visited = mutableSetOf<Coordinate>()
+        while (remainingMoves.isNotEmpty()) {
+            val move = remainingMoves.remove()
+            val whatsHere = map[move.position]
+            if (move.steps > fastestSolveWithCheating) {
+                // No reason to keep trying
+                return null
+            } else if (move.position in move.moves) {
+                // We've already been here
+                continue
+            } else if (whatsHere == null) {
+                continue /// can't walk off the grid
+            } else if (whatsHere == '#') {
+                continue // can't walk into a wall
+            } else if (move.position == endingPoint) {
+                return move.steps
+            } else if (!visited.add(move.position)) {
                 continue // already been here
             }
             remainingMoves.addAll(move.getNextMoves())
@@ -90,7 +152,7 @@ class Day20 {
         val startingPoint = map.filterValues { it == 'S' }.keys.first()
         val endingPoint = map.filterValues { it == 'E' }.keys.first()
 
-        val fastestPathWithoutCheating = solve(map, Move(startingPoint, 0), endingPoint, Int.MAX_VALUE)!!
+        val fastestPathWithoutCheating = solvePath(map, Move(startingPoint, 0), endingPoint, Int.MAX_VALUE)!!
         val fastestTimeWithoutCheating = fastestPathWithoutCheating.size
 
         // Sample input should be 84
@@ -113,7 +175,43 @@ class Day20 {
                 if (timingCache.containsKey(startingMove.position)) {
                     fastestTimeWithoutCheating - timingCache[startingMove.position]!! + startingMove.steps
                 } else {
-                    solve(map, startingMove, endingPoint, fastestTimeWithoutCheating)?.size
+                    solveSteps(map, startingMove, endingPoint, fastestTimeWithoutCheating)
+                }
+            }
+
+        return allSolves.count { fastestTimeWithoutCheating - it >= timeSaved }
+    }
+
+    fun part2(input: List<String>, timeSaved: Int = 100): Int {
+        val map = buildMap(input)
+        val startingPoint = map.filterValues { it == 'S' }.keys.first()
+        val endingPoint = map.filterValues { it == 'E' }.keys.first()
+
+        val fastestPathWithoutCheating = solvePath(map, Move(startingPoint, 0), endingPoint, Int.MAX_VALUE)!!
+        val fastestTimeWithoutCheating = fastestPathWithoutCheating.size
+
+        // Sample input should be 84
+        "Fastest solve without cheat $fastestTimeWithoutCheating".println()
+
+//        prettyPrintGrid(15, 15, map, move.moves)
+
+        // Path is well-defined, so we only need to cheat along the path
+        val newCheatingStartPositions = fastestPathWithoutCheating.flatMapIndexed { index, coordinate ->
+            val recreatedMove = Move(coordinate, index, moves = fastestPathWithoutCheating.take(index))
+            recreatedMove.getMegaCheatMoves()
+        }
+
+        // This is not as good for part 2, since we cheat a lot more, but still worked in <2 minutes
+        val timingCache = fastestPathWithoutCheating
+            .mapIndexed { index, coordinate -> coordinate to index }
+            .toMap(mutableMapOf())
+
+        val allSolves = newCheatingStartPositions
+            .mapNotNull { startingMove ->
+                if (timingCache.containsKey(startingMove.position)) {
+                    fastestTimeWithoutCheating - timingCache[startingMove.position]!! + startingMove.steps
+                } else {
+                    solveSteps(map, startingMove, endingPoint, fastestTimeWithoutCheating)
                 }
             }
 
@@ -126,11 +224,6 @@ fun main() {
 
 
     val day20 = Day20()
-
-
-    fun part2(input: List<String>): Int {
-        TODO("Need to implement Part 2")
-    }
 
     val sampleInput = """
         ###############
@@ -160,8 +253,8 @@ fun main() {
     val input = readInput("2024/Day20")
     day20.part1(input).println()
 
-    check(part2(sampleInput) == 982)
-    part2(input).println()
+    check(day20.part2(sampleInput, 76) == 3)
+    day20.part2(input).println()
 
     "${(System.currentTimeMillis() - start)} milliseconds".println()
 }
