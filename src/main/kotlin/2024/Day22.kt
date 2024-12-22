bd3062e7dc90630987d5aa35ab18a3824e86687b
Scott Busche <swbusche@gmail.com>
2024-12-22 13:26:17 -0600

    Day 22 - Part 2

--- src/main/kotlin/2024/Day22.kt
+++ src/main/kotlin/2024/Day22.kt
@@ -23,21 +23,58 @@ class Day22 {
         fun part1(input: List<String>): Long {
             return input.sumOf { generateEntireDaysSecret(it.toLong()) }
         }
+
+        fun getAllSecretPricesFromSecret(startingSecret: Long): List<Int> {
+            var secret = startingSecret
+            val secretPrices = mutableListOf((startingSecret % 10).toInt())
+            repeat(2000) {
+                secret = generateNextSecret(secret)
+                secretPrices.add((secret % 10).toInt())
+            }
+            return secretPrices
+        }
+
+        fun getIndexedPriceSequences(priceList: List<Int>): Map<List<Int>, Int> {
+            val priceSequences = priceList.mapIndexedNotNull { index, price ->
+                if (index < 4) {
+                    null // Need to have at least 4 to do our manual window
+                } else {
+                    price to listOf(
+                        priceList[index - 3] - priceList[index - 4],
+                        priceList[index - 2] - priceList[index - 3],
+                        priceList[index - 1] - priceList[index - 2],
+                        priceList[index] - priceList[index - 1],
+                    )
+                }
+            }
+
+            return priceSequences.mapIndexedNotNull { index, (price, sequence) ->
+                if (priceSequences.subList(0, index).any { it.second == sequence }) {
+                    null // If we've seen this sequence already don't replace it, off by 2 error otherwise
+                } else sequence to price
+            }.toMap(LinkedHashMap())
+        }
+
+        fun part2(input: List<String>): Int {
+            val allPrices = input.map { getAllSecretPricesFromSecret(it.toLong()) }
+            val allPriceSequences = allPrices.map { getIndexedPriceSequences(it) }
+
+            val uniqueSequences =
+                allPriceSequences.flatMap { it.map { (priceSequence, _) -> priceSequence } }.toSet()
+
+            return uniqueSequences
+                .maxOf { sequence -> allPriceSequences.sumOf { it[sequence] ?: 0 } }
+        }
     }
 }
 
 fun main() {
     val start = System.currentTimeMillis()
 
-    fun part2(input: List<String>): Int {
-        TODO("Need to implement Part 2")
-    }
-
     val input = readInput("2024/Day22")
     Day22.part1(input).println()
 
-//    check(part2(sampleInput) == 982)
-    part2(input).println()
+    Day22.part2(input).println()
 
     "${(System.currentTimeMillis() - start)} milliseconds".println()
 }
