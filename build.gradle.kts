a66065766100519a6a56b8e00060592f9fb01da0
Scott Busche <swbusche@gmail.com>
2025-12-03 18:59:55 -0600

    Upgrade all the things

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,29 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
-    kotlin("plugin.serialization") version "2.1.0"
+    kotlin("jvm") version "2.3.0-RC2"
+    kotlin("plugin.serialization") version "2.3.0-RC2"
 }
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 
     testImplementation(kotlin("test"))
 }
 
 tasks {
     wrapper {
-        gradleVersion = "8.11.1"
+        gradleVersion = "9.2.1"
     }
 }
 
