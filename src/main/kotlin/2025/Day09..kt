82c81f37daf600318407bd4a727c6962b277a7ff
Scott Busche <swbusche@gmail.com>
2025-12-09 21:49:11 -0600

    Day 9 - Part 2 - Framework for where to go ha

--- src/main/kotlin/2025/Day09..kt
+++ src/main/kotlin/2025/Day09..kt
@@ -14,6 +14,7 @@ fun main() {
             Coordinate(x.toInt(), y.toInt())
         }
         return coordinates.combinations().maxOf { (coordinate1, coordinate2) -> 
+            // +1 as it's inclusive, we get to count the whole square
             val length = (coordinate1.x - coordinate2.x).absoluteValue.toLong() + 1
             val width = (coordinate1.y - coordinate2.y).absoluteValue + 1
             length * width
@@ -31,8 +32,37 @@ fun main() {
         7,3
     """.trimIndent()
     check(part1(sampleInput.lines()) == 50L)
+    
+    data class Side(val start: Coordinate, val end: Coordinate) {
+        fun contains(coordinate: Coordinate): Boolean {
+            return coordinate.x in start.x..end.y && coordinate.y in start.y..end.y
+        }
+    }
 
     fun part2(input: List<String>): Long {
+        val coordinates = input.map {
+            val (x, y) = it.split(",")
+            Coordinate(x.toInt(), y.toInt())
+        }
+        // Make all of them into sides, they appear to be ordered?
+        // Need to add the first one again to complete the loop
+        val sides = (coordinates + coordinates.first())
+            .zipWithNext()
+            .map { (coordinate1, coordinate2) -> Side(coordinate1, coordinate2) }
+
+
+        return coordinates.combinations().maxOf { (coordinate1, coordinate2) ->
+            // Confirm the rectangle we make from these coordinates
+            // exists inside the rectangle formed by all the edges
+            // TODO figure out how to make that rectangle
+            
+            val length = (coordinate1.x - coordinate2.x).absoluteValue.toLong() + 1
+            val width = (coordinate1.y - coordinate2.y).absoluteValue + 1
+            length * width
+        }
+        
+        
+        
         TODO()
     }
 
@@ -40,7 +70,7 @@ fun main() {
     val input = readInput("2025/Day09")
     part1(input).println()
 
-    check(part2(sampleInput.lines()) == 25272L)
+    check(part2(sampleInput.lines()) == 15L)
 
     part2(input).println()
 }
