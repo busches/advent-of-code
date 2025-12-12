3ff0a4fe146036585302ab500e9c671b62eaca07
Scott Busche <swbusche@gmail.com>
2025-12-11 21:47:02 -0600

    Day 11 - Part 2 - ugh caching

--- src/main/kotlin/2025/Day11.kt
+++ src/main/kotlin/2025/Day11.kt
@@ -1,9 +1,7 @@
 package `2025`
 
-import utils.anyIndexed
 import utils.println
 import utils.readInput
-import java.util.PriorityQueue
 
 fun main() {
 
@@ -57,14 +55,60 @@ fun main() {
     """.trimIndent()
     check(part1(sampleInput.lines()) == 5L)
 
+    data class Part2(val startDevice: String, val visitedDAC: Boolean, val visitedFFT: Boolean)
+
     fun part2(input: List<String>): Long {
-        TODO()
+        val devices = input.associate { line ->
+            val device = line.take(3)
+            val outputs = line.drop(5).split(' ')
+            device to outputs
+        }
+
+        val start = "svr"
+        val end = "out"
+        val memo = mutableMapOf<Part2, Long>()
+        fun findNextPath(path: List<String>, nextDevice: String, visitedDAC: Boolean, visitedFFT: Boolean): Long {
+            return memo.getOrPut(Part2(nextDevice, visitedDAC, visitedFFT)) {
+                return@getOrPut if (nextDevice == end) {
+                    if (visitedDAC && visitedFFT) 1 else 0
+                } else {
+                    val path = path + nextDevice
+                    val visitedDAC = visitedDAC || nextDevice == "dac"
+                    val visitedFFT = visitedFFT || nextDevice == "fft"
+                    devices[nextDevice]!!.fold(0L) { acc, nextDevice ->
+                        acc + findNextPath(
+                            path = path,
+                            nextDevice = nextDevice,
+                            visitedDAC = visitedDAC,
+                            visitedFFT = visitedFFT,
+                        )
+                    }
+                }
+            }
+        }
+        return findNextPath(path = emptyList(), nextDevice = start, visitedDAC = false, visitedFFT = false)
     }
 
     val input = readInput("2025/Day11")
     part1(input).println()
 
-    check(part2(sampleInput.lines()) == 33L)
+    val sampleInput2 = """
+        svr: aaa bbb
+        aaa: fft
+        fft: ccc
+        bbb: tty
+        tty: ccc
+        ccc: ddd eee
+        ddd: hub
+        hub: fff
+        eee: dac
+        dac: fff
+        fff: ggg hhh
+        ggg: out
+        hhh: out
+    """.trimIndent()
+
+    check(part2(sampleInput2.lines()) == 2L)
 
     part2(input).println()
 }
